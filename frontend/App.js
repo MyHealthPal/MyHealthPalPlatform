a88e6264a00a5fedc4e66a1017a9f6481187ce26
Sathursan <ganasha02@gmail.com>
2021-05-30 12:40:30 -0400

    FRONTEND WORKS

---
+++ frontend/App.js
@@ -0,0 +1,34 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const getList = async () => {
+  try {
+    let response = await fetch(
+      'https://localhost:5000/list'
+    );
+    let json = await response.json();
+    console.log(json);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+getList();
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
