2d6c8f066f0e0639c74c8780dc334997bc627520
Preyansh <preyanshkachhiapk@gmail.com>
2021-06-08 23:39:02 -0400

    first commit, added boilerplate for navbar component

---
+++ frontend/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const Navbar = () => {
+    return (
+        <View>
+            <Text>Hello Navbar!</Text> 
+        </View>
+    )
+}
+
+export default Navbar
