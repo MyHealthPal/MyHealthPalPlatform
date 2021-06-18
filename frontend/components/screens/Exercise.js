cb43b109a7b015bade6431a614e8d2df4eaa420b
RishanRatna <rishanratna@hotmail.com>
2021-06-18 19:50:47 -0400

    removed comment

--- frontend/components/screens/Exercise.js
+++ frontend/components/screens/Exercise.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import CustomButton from '../button/custom-button';
-import Toast from 'react-native-toast-message';
 
 const Exercise = () => {
   return (
