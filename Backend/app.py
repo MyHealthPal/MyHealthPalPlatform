7651f59c6a9ed61ce9bed6b03971b86a62819246
adshayan <adshayan.b@gmail.com>
2021-07-01 12:40:53 -0400

    Casanadra Intergation

--- Backend/app.py
+++ Backend/app.py
@@ -7,11 +7,37 @@ from flask import Flask, request
 from flask_cors import CORS
 from functools import wraps
 from firebase_admin import firestore 
-
-
-#App configuration
+import uuid
+from flask import Flask
+from flask_cqlalchemy import CQLAlchemy
 app = Flask(__name__)
 CORS(app)
+
+app.config['CASSANDRA_HOSTS'] = ['127.0.0.1']
+app.config['CASSANDRA_KEYSPACE'] = "cqlengine"
+db = CQLAlchemy(app)
+
+
+class VaccinationPassport(db.Model):
+    id = db.columns.UUID(primary_key=True, default=uuid.uuid4)
+    public_id = db.columns.Text()
+    first_name  = db.columns.Text()
+    last_name = db.columns.Text()
+    health_card = db.columns.Text()
+    date_of_birth = db.columns.Date()
+    date_of_dose = db.columns.Date()
+    agent = db.columns.Text()
+    product_name = db.columns.Text()
+    diluent_product = db.columns.Text()
+    lot = db.columns.Text()
+    dosage = db.columns.Text()
+    route = db.columns.Text()
+    site = db.columns.Text()
+    dose = db.columns.Integer()
+    organization = db.columns.Text()
+
+db.sync_db()
+
 
 #Connect to firebase
 cred = credentials.Certificate('key.json')
@@ -82,6 +108,33 @@ def getAccountInfo():
     email= request.json['email']
     info = pb.auth().send_password_reset_email(email)
     return info 
+
+@app.route('/api/AddVaccine', methods=['POST'])
+@TokenRequired
+def addVaccine():
+    data = request.json
+    newVaccination = VaccinationPassport(public_id = request.user['uid'], first_name = data['firstName'], last_name = data['lastName'] ,
+     health_card = data['healthCard'], date_of_birth = data['DateOfBirth'], date_of_dose  = data['DateOfDose'], agent = data['agent'],
+    product_name = data['productName'], diluent_product = data["DiluentProduct"], lot = data['lot'], dosage = data['dosage'], route = data['route'],
+    site = data['site'], dose = data['dose'], organization = data['org'])
+    newVaccination.save()
+
+    return {"message":"Vaccine was added"},200
+    
+    
+    
+    return "work in progress"
+    # product_name = db.columns.Text()
+    # diluent_product = db.columns.Text()
+    # lot = db.columns.Text()
+    # dosage = db.columns.Text()
+    # route = db.columns.Text()
+    # site = db.columns.Text()
+    # dose = db.columns.Integer()
+    # organization = db.columns.Tex
+
+
+
 
 
 if __name__ == '__main__':
