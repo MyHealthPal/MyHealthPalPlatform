8496bc41d0325d41f920d8618b9c4becf21212c4
RishanRatna <rishanratna@hotmail.com>
2021-08-09 23:10:57 -0400

    Completed vaccination details info page

--- frontend/screens/healthScreens/VaccinationInfo.js
+++ frontend/screens/healthScreens/VaccinationInfo.js
@@ -1,22 +1,17 @@
 import React, { useEffect, useContext, useState } from 'react';
-import {
-  View,
-  Text,
-  SafeAreaView,
-  StyleSheet,
-  StatusBar,
-  FlatList,
-} from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 import { MainContext } from '../../context/MainContext';
 import Toast from 'react-native-toast-message';
 import * as SecureStore from 'expo-secure-store';
+import CustomCard from '../../components/card/custom-card';
+import IconBadge from '../../components/iconBadge/custom-iconBadge';
+import CustomPopupAlert from '../../components/alerts/custom-popup-alert';
 
-const VaccinationInfo = ({ id }) => {
+const VaccinationInfo = ({ navigation, id }) => {
   const context = useContext(MainContext);
 
-  const statusHeight = StatusBar.currentHeight;
-
-  const [vaccineInfo, setVaccineInfo] = useState(null);
+  const [vaccineInfo, setVaccineInfo] = useState({});
+  const [showConfirmDelete, setShowConfirmDelete] = useState(false);
 
   const getToken = () => {
     return SecureStore.getItemAsync('auth_token');
@@ -26,7 +21,46 @@ const VaccinationInfo = ({ id }) => {
     return word.charAt(0).toUpperCase() + word.slice(1);
   };
 
+  const handleEdit = () => {
+    // Navigate to the edit page and send the vaccination details as a param
+  };
+
+  const handleDelete = () => {
+    setShowConfirmDelete(true);
+  };
+
+  const handleConfirmDelete = () => {
+    setShowConfirmDelete(false);
+    // Delete vaccine api request and navigate back to vaccination records page when finished
+  };
+
   const pageContainerClass = 'pageContainer' + capitalize(context.theme);
+
+  const vaccineDetailsOrder = [
+    'date_of_dose',
+    'agent',
+    'product_name',
+    'diluent_product',
+    'lot',
+    'dosage',
+    'route',
+    'site',
+    'dose',
+    'organization',
+  ];
+
+  const vaccineDetailToIconMap = {
+    product_name: { name: 'syringe', library: 'FontAwesome5' },
+    date_of_dose: { name: 'calendar-alt', library: 'FontAwesome5' },
+    organization: { name: 'organization', library: 'SimpleLineIcons' },
+    agent: { name: 'virus', library: 'FontAwesome5' },
+    diluent_product: { name: 'drop', library: 'Entypo' },
+    dosage: { name: 'eyedropper', library: 'MaterialCommunityIcons' },
+    route: { name: 'route', library: 'FontAwesome5' },
+    site: { name: 'location-pin', library: 'Entypo' },
+    lot: { name: 'pin', library: 'Entypo' },
+    dose: { name: 'counter', library: 'MaterialCommunityIcons' },
+  };
 
   const fetchVaccineInfo = async () => {
     let response;
@@ -53,38 +87,175 @@ const VaccinationInfo = ({ id }) => {
           type: 'error',
         });
       } else {
-        console.log(Object.entries(json));
-        setVaccineInfo(Object.entries(json));
+        setVaccineInfo(json);
       }
     });
   };
+
+  const renderVaccineInfo = ({ item, index }) => (
+    <CustomCard
+      outerStyle={[
+        styles.vaccineDetailsOuterContainer,
+        index === 0 ? { marginTop: 16 } : {},
+        index === vaccineDetailsOrder.length - 1 ? { marginBottom: 16 } : {},
+      ]}
+      innerStyle={styles.vaccineDetailsInnerContainer}
+      noTouchOpacity
+    >
+      <View style={styles.vaccineDetailsIcon}>
+        <IconBadge
+          noTouchOpacity
+          library={vaccineDetailToIconMap[item.category]?.library ?? ''}
+          icon={vaccineDetailToIconMap[item.category]?.name ?? ''}
+          size={45}
+          color={context.theme === 'dark' ? '#404040' : '#e0e0e0'}
+        />
+      </View>
+      <View style={styles.vaccineDetailsTextContainer}>
+        <Text
+          style={[
+            styles.vaccineDetailsTitle,
+            context.theme === 'dark'
+              ? { color: '#ffffff' }
+              : { color: '#212121' },
+          ]}
+        >
+          {item.category
+            .split('_')
+            .map((word) => capitalize(word))
+            .join(' ')}
+        </Text>
+        <Text
+          style={[
+            styles.vaccineDetailsValue,
+            context.theme === 'dark'
+              ? { color: '#d4d4d4' }
+              : { color: '#606060' },
+          ]}
+        >
+          {item.value}
+        </Text>
+      </View>
+    </CustomCard>
+  );
 
   useEffect(() => {
     fetchVaccineInfo();
   }, []);
 
+  useEffect(() => {
+    if (vaccineInfo) {
+      navigation.setOptions({
+        title: vaccineInfo.product_name,
+        headerRight: () => (
+          <View style={styles.headerActions}>
+            <IconBadge
+              library="FontAwesome5"
+              icon="edit"
+              size={25}
+              color="#ffffff"
+              style={styles.headerButton}
+              onPress={handleEdit}
+            />
+            <IconBadge
+              library="FontAwesome5"
+              icon="trash-alt"
+              size={25}
+              color="#ffffff"
+              style={styles.headerButton}
+              onPress={handleDelete}
+            />
+          </View>
+        ),
+      });
+    }
+  }, [vaccineInfo]);
+
   return (
-    <SafeAreaView
-      style={(styles[pageContainerClass], { marginTop: statusHeight })}
-    >
-      <Text>{vaccineInfo ? 'Found info' : 'User does not exist.'}</Text>
-      {/* <FlatList
-        style={styles.flatlistContainer}
-        data={vaccineInfo}
-        renderItem={renderItem}
-      /> */}
-    </SafeAreaView>
+    <View style={styles[pageContainerClass]}>
+      <FlatList
+        data={vaccineDetailsOrder.map((vaccineCategory, key) => ({
+          id: key,
+          category: vaccineCategory,
+          value: vaccineInfo[vaccineCategory],
+        }))}
+        renderItem={renderVaccineInfo}
+        keyExtractor={(item) => item.id.toString()}
+      />
+      <CustomPopupAlert
+        open={showConfirmDelete}
+        color={'#FC3636'}
+        icon={'trash-alt'}
+        iconLibrary={'FontAwesome5'}
+        iconSize={40}
+        title="Confirm Delete"
+        description="Are you sure you want to delete this record?"
+        buttons={[
+          {
+            text: 'Cancel',
+            type: 'outlined',
+            onPress: () => setShowConfirmDelete(false),
+          },
+          {
+            text: 'Delete',
+            type: 'regular',
+            backgroundColor: '#FC3636',
+            onPress: handleConfirmDelete,
+          },
+        ]}
+      />
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   pageContainerLight: {
-    backgroundColor: '#ffffff',
+    backgroundColor: '#F8F8F8',
     height: '100%',
   },
   pageContainerDark: {
-    backgroundColor: '#212121',
+    backgroundColor: '#000000',
     height: '100%',
+  },
+  headerActions: {
+    marginRight: 12,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  headerButton: {
+    marginHorizontal: 8,
+  },
+  vaccineDetailsOuterContainer: {
+    marginVertical: 8,
+    marginHorizontal: 20,
+  },
+  vaccineDetailsInnerContainer: {
+    padding: 20,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+    position: 'relative',
+  },
+  vaccineDetailsTextContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'flex-start',
+    flex: 1,
+  },
+  vaccineDetailsTitle: {
+    fontSize: 18,
+    fontFamily: 'Oxygen-Bold',
+  },
+  vaccineDetailsValue: {
+    fontSize: 18,
+    fontFamily: 'Oxygen-Regular',
+  },
+  vaccineDetailsIcon: {
+    marginRight: 20,
   },
 });
 
