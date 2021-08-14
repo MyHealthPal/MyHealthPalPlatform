0c429563b93766e35dd57efceda18087f1ec68de
RishanRatna <rishanratna@hotmail.com>
2021-08-14 16:12:35 -0400

    quick fix

---
+++ frontend/screens/prescriptionScreens/PrescriptionInfo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const PrescriptionInfo = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default PrescriptionInfo;
