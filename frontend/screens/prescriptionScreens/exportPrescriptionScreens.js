d68fd847f5e63bf4a7c8a05532c5f661f8ff612e
RishanRatna <rishanratna@hotmail.com>
2021-08-14 12:01:41 -0400

    Changed the folder structure of prescriptions

---
+++ frontend/screens/prescriptionScreens/exportPrescriptionScreens.js
@@ -0,0 +1,27 @@
+import React, { useContext } from 'react';
+import PrescriptionInfo from './PrescriptionInfo';
+import PrescriptionTracking from './PrescriptionTracking';
+import { Stack } from '../../context/MainContext';
+
+function PrescriptionScreens() {
+  return (
+    <>
+      <Stack.Screen
+        name="PrescriptionTracking"
+        component={PrescriptionTracking}
+      />
+      <Stack.Screen
+        options={{
+          headerTintColor: '#ffffff',
+          headerStyle: {
+            backgroundColor: '#f62e4a',
+          },
+        }}
+        name="PrescriptionInfo"
+        component={PrescriptionInfo}
+      ></Stack.Screen>
+    </>
+  );
+}
+
+export default PrescriptionScreens;
