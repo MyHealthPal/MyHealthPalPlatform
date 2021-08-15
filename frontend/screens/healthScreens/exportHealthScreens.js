f16e25e97018db9809294b6529a5f9d5d98c0181
RishanRatna <rishanratna@hotmail.com>
2021-08-15 14:43:51 -0400

    Connected vaccination screens, changed date of dose in vaccination passport schema to be a Text field instead of Date, and changed date format of vaccination info and vaccination records to be human readable format

--- frontend/screens/healthScreens/exportHealthScreens.js
+++ frontend/screens/healthScreens/exportHealthScreens.js
@@ -1,38 +1,15 @@
 import React, { useContext } from 'react';
-import VaccinationRecords from './VaccinationRecords';
 import VisionTest from './VisionTest';
-import VaccinationInfo from './VaccinationInfo';
 import PrescriptionScreens from '../prescriptionScreens/exportPrescriptionScreens';
 import { Stack } from '../../context/MainContext';
+import VaccinationScreens from '../vaccinationScreens/exportVaccinationScreens';
 
 function HealthScreens() {
   return (
     <>
-      <Stack.Screen
-        options={{
-          headerTintColor: '#ffffff',
-          headerStyle: {
-            backgroundColor: '#f62e4a',
-          },
-        }}
-        name="VaccinationInfo"
-        component={VaccinationInfo}
-      ></Stack.Screen>
-      <Stack.Screen
-        options={{
-          // headerTransparent: false,
-          headerStyle: {
-            backgroundColor: '#f62e4a',
-          },
-          headerBackTitle: 'Health',
-          headerTintColor: '#ffffff',
-          title: 'Vaccination Records',
-        }}
-        name="VaccinationRecords"
-        component={VaccinationRecords}
-      />
-      <Stack.Screen name="VisionTest" component={VisionTest} />
+      {VaccinationScreens()}
       {PrescriptionScreens()}
+      <Stack.Screen name="VisionTest" component={VisionTest} />
     </>
   );
 }
