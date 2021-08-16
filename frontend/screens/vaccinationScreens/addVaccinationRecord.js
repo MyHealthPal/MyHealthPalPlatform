48dc2617f579a0908aedccf5451219ceba94b970
Sathursan <ganasha02@gmail.com>
2021-08-15 21:26:08 -0400

    Add and Update Pages Finished

--- frontend/screens/vaccinationScreens/addVaccinationRecord.js
+++ frontend/screens/vaccinationScreens/addVaccinationRecord.js
@@ -11,16 +11,17 @@ import CustomHeader from "../../components/header/custom-header";
 import CustomInputBox from "../../components/inputBox/custom-inputBox";
 import CustomButton from "../../components/button/custom-button";
 import DateTimePickerModal from "react-native-modal-datetime-picker";
+import IconBadge from "../../components/iconBadge/custom-iconBadge";
 import Toast from "react-native-toast-message";
 
 const AddUpdateVaccinationRecords = ({ update, recordId }) => {
   const token =
-    "eyJhbGciOiJSUzI1NiIsImtpZCI6ImFlMDVlZmMyNTM2YjJjZTdjNTExZjRiMTcyN2I4NTkyYTc5ZWJiN2UiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vbXktaGVhbHRoLXBhbC1kZjQxYSIsImF1ZCI6Im15LWhlYWx0aC1wYWwtZGY0MWEiLCJhdXRoX3RpbWUiOjE2Mjg4MjQ3OTEsInVzZXJfaWQiOiJ4UzZNTXV5WENDWTVlR05tS2RvZnRrY3B6MjMzIiwic3ViIjoieFM2TU11eVhDQ1k1ZUdObUtkb2Z0a2NwejIzMyIsImlhdCI6MTYyODgyNDc5MSwiZXhwIjoxNjI4ODI4MzkxLCJlbWFpbCI6InJpc2hhbnJhdG5hQGhvdG1haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImZpcmViYXNlIjp7ImlkZW50aXRpZXMiOnsiZW1haWwiOlsicmlzaGFucmF0bmFAaG90bWFpbC5jb20iXX0sInNpZ25faW5fcHJvdmlkZXIiOiJwYXNzd29yZCJ9fQ.FBwx0-OgIK0LWoNHTW-WU7_vl6osb6cbKCrT8_acNB5R-c_AlK2TFXsrVrz0sEDRDILKBdq400OZsztxghoyWU2dYCLCPsgTXRBuhrWT0KnEoug6kQMC4dcBCrorSOHbybpV7v6S5DegNwEGJWOhy133zI89NUyG1CEA7-6kP46E0wO5XfnbLG0d8zTA_tWpCkBX8TVvUDpynLAxBD54Ucz-RdNy4obWh1Ip5mqLgeMPCEeuZ9TIRwmTtbR74flNjqyg-5i0s10cx0E1xMOcWXtZf2dVyGjlafKGqtIjrjdIlhkPfZpQTQP7D9_fEJczMgfbqPgs-bm1Dac1w0vZJQ";
+    "eyJhbGciOiJSUzI1NiIsImtpZCI6IjM2NGU4NTQ1NzI5OWQ5NzIxYjczNDQyZGNiNTQ3Y2U2ZDk4NGRmNTkiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vbXktaGVhbHRoLXBhbC1kZjQxYSIsImF1ZCI6Im15LWhlYWx0aC1wYWwtZGY0MWEiLCJhdXRoX3RpbWUiOjE2MjkwNzQ5NzEsInVzZXJfaWQiOiJ4UzZNTXV5WENDWTVlR05tS2RvZnRrY3B6MjMzIiwic3ViIjoieFM2TU11eVhDQ1k1ZUdObUtkb2Z0a2NwejIzMyIsImlhdCI6MTYyOTA3NDk3MSwiZXhwIjoxNjI5MDc4NTcxLCJlbWFpbCI6InJpc2hhbnJhdG5hQGhvdG1haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImZpcmViYXNlIjp7ImlkZW50aXRpZXMiOnsiZW1haWwiOlsicmlzaGFucmF0bmFAaG90bWFpbC5jb20iXX0sInNpZ25faW5fcHJvdmlkZXIiOiJwYXNzd29yZCJ9fQ.FSUq__5dhukERV3Zq_TdiKdqHRYNavsfibbOUL7RnL0x2jDhiPds8pARGQFaItwDpHR7Iet-UWztRoP0lZMSO_roVukYBWutC_lZdKM1a1mKzDs0Q3kV5bm0A5kZ7iT992AsMMM38EyPNoLom15wRa1oLH6MmAHd9LY6vpVSJj_FdxvWij0rlmYZB6_sF0DbMT2no6UgmYwwRkTzIg9A6YRFuH-z2MbAfIF8R11L_0zPTaB7Ubm9A3JW7R5_1GkQp8cCJu5HoHyXJAgRqMk-QqY12vF6e-YQvFoKRH41_MWyYhyenRhNXyrHe_HajvY3EC-PRcaynL-YfIkdmQKvVw";
   const context = useContext(MainContext);
   const [showDatePicker, setShowDatePicker] = useState(false);
   const [vaccineData, setVaccineData] = useState({
     agent: "",
-    date_of_dose: undefined,
+    date_of_dose: "2021-10-06",
     diluent_product: "",
     dosage: "",
     dose: "",
@@ -30,8 +31,21 @@ const AddUpdateVaccinationRecords = ({ update, recordId }) => {
     product_name: "",
     route: "",
     site: "",
-    image: "",
+    //image: "",
   });
+
+  const vaccineDetailToIconMap = {
+    product_name: { name: "syringe", library: "FontAwesome5" },
+    date_of_dose: { name: "calendar-alt", library: "FontAwesome5" },
+    organization: { name: "organization", library: "SimpleLineIcons" },
+    agent: { name: "virus", library: "FontAwesome5" },
+    diluent_product: { name: "drop", library: "Entypo" },
+    dosage: { name: "eyedropper", library: "MaterialCommunityIcons" },
+    route: { name: "route", library: "FontAwesome5" },
+    site: { name: "location-pin", library: "Entypo" },
+    lot: { name: "pin", library: "Entypo" },
+    dose: { name: "counter", library: "MaterialCommunityIcons" },
+  };
 
   useEffect(() => {
     if (update) {
@@ -42,6 +56,7 @@ const AddUpdateVaccinationRecords = ({ update, recordId }) => {
   }, []);
 
   const fetchVaccineDetails = async () => {
+    //GET TOKEN
     let response;
     response = await fetch(context.fetchPath + "api/getVaccine/" + recordId, {
       method: "GET",
@@ -77,13 +92,39 @@ const AddUpdateVaccinationRecords = ({ update, recordId }) => {
   const onChange = (value, key) => {
     let temp = vaccineData;
     temp[key] = value.toString();
-    console.log(temp);
     setVaccineData({ ...temp });
   };
 
-  const addUpdate = () => {
-    console.log("Backend Call");
-    console.log(vaccineData);
+  const addUpdate = async () => {
+    //validation REQUIRED FIELDS
+    //GET TOKEN
+    if (update) {
+      let response;
+      let json;
+      response = await fetch(context.fetchPath + "api/updateVaccine", {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          "x-access-tokens": token,
+        },
+        body: JSON.stringify(vaccineData),
+      });
+
+      json = await response.json();
+    } else {
+      let response;
+      let json;
+      response = await fetch(context.fetchPath + "api/addVaccine", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "x-access-tokens": token,
+        },
+        body: JSON.stringify(vaccineData),
+      });
+
+      json = await response.json();
+    }
   };
 
   const goBack = () => {
@@ -112,13 +153,26 @@ const AddUpdateVaccinationRecords = ({ update, recordId }) => {
                   onPress={() => setShowDatePicker(true)}
                   style={{ width: "100%" }}
                 >
-                  <View pointerEvents="none">
+                  <View style={styles.rowContainer} pointerEvents="none">
+                    <IconBadge
+                      noTouchOpacity
+                      library={
+                        vaccineDetailToIconMap["date_of_dose"]?.library ?? ""
+                      }
+                      icon={vaccineDetailToIconMap["date_of_dose"]?.name ?? ""}
+                      size={45}
+                      color={context.theme === "dark" ? "#404040" : "#e0e0e0"}
+                    />
                     <CustomInputBox
                       field="Date of Dose"
                       placeholder="Select the date of dose"
                       value={vaccineData["date_of_dose"] ?? ""}
                       onChange={setVaccineData}
-                      containerStyling={{ marginVertical: 20 }}
+                      containerStyling={{
+                        flex: 1,
+                        marginVertical: 20,
+                        marginLeft: 20,
+                      }}
                     />
                   </View>
                 </TouchableOpacity>
@@ -127,19 +181,32 @@ const AddUpdateVaccinationRecords = ({ update, recordId }) => {
               return <></>;
             } else {
               return (
-                <CustomInputBox
-                  key={key}
-                  field={key
-                    .split("_")
-                    .map((word) => capitalize(word))
-                    .join(" ")}
-                  placeholder={"Enter the " + key.split("_").join(" ")}
-                  value={vaccineData[key]}
-                  onChange={(value) => {
-                    onChange(value, key);
-                  }}
-                  containerStyling={{ marginVertical: 20 }}
-                />
+                <View style={styles.rowContainer}>
+                  <IconBadge
+                    noTouchOpacity
+                    library={vaccineDetailToIconMap[key]?.library ?? ""}
+                    icon={vaccineDetailToIconMap[key]?.name ?? ""}
+                    size={45}
+                    color={context.theme === "dark" ? "#404040" : "#e0e0e0"}
+                  />
+                  <CustomInputBox
+                    key={key}
+                    field={key
+                      .split("_")
+                      .map((word) => capitalize(word))
+                      .join(" ")}
+                    placeholder={"Enter the " + key.split("_").join(" ")}
+                    value={vaccineData[key]}
+                    onChange={(value) => {
+                      onChange(value, key);
+                    }}
+                    containerStyling={{
+                      flex: 1,
+                      marginVertical: 20,
+                      marginLeft: 20,
+                    }}
+                  />
+                </View>
               );
             }
           })}
@@ -203,6 +270,11 @@ const styles = StyleSheet.create({
     alignItems: "center",
     marginRight: 20,
     marginLeft: 20,
+  },
+  rowContainer: {
+    display: "flex",
+    flexDirection: "row",
+    width: "100%",
   },
   submitWrapper: {
     display: "flex",
