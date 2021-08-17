5b40bacb4b91df6efe8b3593630c522a73674d48
Preyansh <preyanshkachhiapk@gmail.com>
2021-08-17 19:45:01 -0400

    completed prescription tracking screen, and made minor changes in Health.js and export files

--- frontend/screens/prescriptionScreens/exportPrescriptionScreens.js
+++ frontend/screens/prescriptionScreens/exportPrescriptionScreens.js
@@ -1,20 +1,27 @@
-import React, { useContext } from 'react';
-import PrescriptionInfo from './PrescriptionInfo';
-import PrescriptionTracking from './PrescriptionTracking';
-import { Stack } from '../../context/MainContext';
+import React, { useContext } from "react";
+import PrescriptionInfo from "./PrescriptionInfo";
+import PrescriptionTracking from "./PrescriptionTracking";
+import { Stack } from "../../context/MainContext";
 
 function PrescriptionScreens() {
   return (
     <>
       <Stack.Screen
+        options={{
+          headerTintColor: "#ffffff",
+          headerStyle: {
+            backgroundColor: "#f62e4a",
+          },
+          title: "Prescription Tracker",
+        }}
         name="PrescriptionTracking"
         component={PrescriptionTracking}
       />
       <Stack.Screen
         options={{
-          headerTintColor: '#ffffff',
+          headerTintColor: "#ffffff",
           headerStyle: {
-            backgroundColor: '#f62e4a',
+            backgroundColor: "#f62e4a",
           },
         }}
         name="PrescriptionInfo"
--- frontend/screens/vaccinationScreens/VaccinationRecords.js
+++ frontend/screens/vaccinationScreens/VaccinationRecords.js
@@ -1,10 +1,10 @@
-import React, { useEffect, useContext, useState } from 'react';
-import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
-import CustomCard from '../../components/card/custom-card';
-import { MainContext } from '../../context/MainContext';
-import IconBadge from '../../components/iconBadge/custom-iconBadge';
-import { humanDateString } from '../../utils/string-utils';
-import * as SecureStore from 'expo-secure-store';
+import React, { useEffect, useContext, useState } from "react";
+import { StyleSheet, Text, View, Image, ScrollView } from "react-native";
+import CustomCard from "../../components/card/custom-card";
+import { MainContext } from "../../context/MainContext";
+import IconBadge from "../../components/iconBadge/custom-iconBadge";
+import { humanDateString } from "../../utils/string-utils";
+import * as SecureStore from "expo-secure-store";
 
 const VaccinationRecords = ({ navigation }) => {
   const [vaccineList, setVaccineList] = useState({});
@@ -15,13 +15,13 @@ const VaccinationRecords = ({ navigation }) => {
     return word.charAt(0).toUpperCase() + word.slice(1);
   };
 
-  const containerClass = 'container' + capitalize(context.theme);
-  const text2Class = 'text2' + capitalize(context.theme);
+  const containerClass = "container" + capitalize(context.theme);
+  const text2Class = "text2" + capitalize(context.theme);
 
-  const iconColor = context.theme == 'dark' ? '#484848' : '#e0e0e0';
+  const iconColor = context.theme == "dark" ? "#484848" : "#e0e0e0";
 
   const getToken = () => {
-    return SecureStore.getItemAsync('auth_token');
+    return SecureStore.getItemAsync("auth_token");
   };
 
   const handleAddVaccine = () => {
@@ -34,10 +34,10 @@ const VaccinationRecords = ({ navigation }) => {
 
     getToken().then(async (token) => {
       response = await fetch(context.fetchPath + `api/getVaccineAll`, {
-        method: 'GET',
+        method: "GET",
         headers: {
-          'Content-Type': 'application/json',
-          'x-access-tokens': token,
+          "Content-Type": "application/json",
+          "x-access-tokens": token,
         },
       });
 
@@ -45,9 +45,9 @@ const VaccinationRecords = ({ navigation }) => {
 
       if (json.message) {
         Toast.show({
-          text1: 'Error',
+          text1: "Error",
           text2: json.message,
-          type: 'error',
+          type: "error",
         });
       } else {
         setVaccineList(json);
@@ -58,19 +58,19 @@ const VaccinationRecords = ({ navigation }) => {
   useEffect(() => {
     fetchVaccineList();
     navigation.setOptions({
-        headerRight: () => (
-          <View style={styles.headerActions}>
-            <IconBadge
-              library="Feather"
-              icon="file-plus"
-              size={25}
-              color="#ffffff"
-              style={styles.headerButton}
-              onPress={handleAddVaccine}
-            />
-          </View>
-        ),
-      });
+      headerRight: () => (
+        <View style={styles.headerActions}>
+          <IconBadge
+            library="Feather"
+            icon="file-plus"
+            size={25}
+            color="#ffffff"
+            style={styles.headerButton}
+            onPress={handleAddVaccine}
+          />
+        </View>
+      ),
+    });
   }, []);
 
   return (
@@ -81,8 +81,8 @@ const VaccinationRecords = ({ navigation }) => {
             <CustomCard
               outerStyle={styles.outterCardStyling}
               onPress={() =>
-                navigation.navigate('VaccinationInfo', {
-                  id: vaccineList[key]['id'],
+                navigation.navigate("VaccinationInfo", {
+                  id: vaccineList[key]["id"],
                 })
               }
             >
@@ -92,12 +92,12 @@ const VaccinationRecords = ({ navigation }) => {
                     <IconBadge
                       library="FontAwesome5"
                       noTouchOpacity={true}
-                      color={'#ff8d4f'}
-                      size={25}
-                      icon={'syringe'}
+                      color={"#ff8d4f"}
+                      size={30}
+                      icon={"syringe"}
                     />
                   </View>
-                  <Text style={styles.text1}>{vaccineList[key]['agent']}</Text>
+                  <Text style={styles.text1}>{vaccineList[key]["agent"]}</Text>
                 </View>
 
                 <View style={styles.textWrapper}>
@@ -106,12 +106,12 @@ const VaccinationRecords = ({ navigation }) => {
                       noTouchOpacity={true}
                       color={iconColor}
                       size={30}
-                      icon={'calendar-month-outline'}
+                      icon={"calendar-month-outline"}
                     />
                   </View>
                   <Text style={[styles.text2, styles[text2Class]]}>
                     {humanDateString(
-                      new Date(vaccineList[key]['date_of_dose'])
+                      new Date(vaccineList[key]["date_of_dose"])
                     )}
                   </Text>
                 </View>
@@ -121,11 +121,11 @@ const VaccinationRecords = ({ navigation }) => {
                       noTouchOpacity={true}
                       color={iconColor}
                       size={30}
-                      icon={'map-marker'}
+                      icon={"map-marker"}
                     />
                   </View>
                   <Text style={[styles.text2, styles[text2Class]]}>
-                    {vaccineList[key]['organization']}
+                    {vaccineList[key]["organization"]}
                   </Text>
                 </View>
               </View>
@@ -142,60 +142,60 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   containerLight: {
-    backgroundColor: '#F8F8F8',
+    backgroundColor: "#F8F8F8",
   },
   containerDark: {
-    backgroundColor: '#000000',
+    backgroundColor: "#000000",
   },
   headerActions: {
     marginRight: 12,
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
   },
   headerButton: {
     marginHorizontal: 8,
   },
   wrapper: {
-    display: 'flex',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'flex-start',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "flex-start",
+    alignItems: "center",
     margin: 20,
   },
   outterCardStyling: {
     marginVertical: 10,
-    width: '100%',
+    width: "100%",
   },
   iconWrapper: { marginRight: 15 },
   text1: {
-    color: '#ff8d4f',
-    fontFamily: 'Oxygen-Bold',
+    color: "#ff8d4f",
+    fontFamily: "Oxygen-Bold",
     fontSize: 20,
   },
   text2: {
-    fontFamily: 'Oxygen-Regular',
+    fontFamily: "Oxygen-Regular",
     fontSize: 15,
   },
   text2Dark: {
-    color: '#d1d1d1',
+    color: "#d1d1d1",
   },
   text2Light: {
-    color: '#7e7e7e',
+    color: "#7e7e7e",
   },
   innerContainer: {
-    display: 'flex',
-    width: '100%',
-    justifyContent: 'center',
+    display: "flex",
+    width: "100%",
+    justifyContent: "center",
     padding: 15,
   },
   textWrapper: {
-    width: '100%',
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
+    width: "100%",
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
   },
 });
 
