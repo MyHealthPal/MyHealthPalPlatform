98ce4ff2723db83310ae4eb597046261cd313020
RishanRatna <rishanratna@hotmail.com>
2021-08-24 23:36:02 -0400

    Fixed env and changed app.py

--- frontend/context/MainContext.js
+++ frontend/context/MainContext.js
@@ -1,12 +1,12 @@
-import React, { createContext, useState } from "react";
-import { createStackNavigator } from "@react-navigation/stack";
+import React, { createContext, useState } from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
 export const Stack = createStackNavigator();
 export const MainContext = createContext();
 
 export const MainProvider = ({ children }) => {
-  const [theme, setTheme] = useState("dark");
-  const [gradient, setGradient] = useState(["#f62e4a", "#ff8d4f"]);
-  const fetchPath = "http://192.168.2.15:5000/";
+  const [theme, setTheme] = useState('light');
+  const [gradient, setGradient] = useState(['#f62e4a', '#ff8d4f']);
+  const fetchPath = 'http://10.0.0.120:5000/';
 
   return (
     <MainContext.Provider
