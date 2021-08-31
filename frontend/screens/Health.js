8854316f07a3d85e577b4141681f557588f38fa2
Preyansh <preyanshkachhiapk@gmail.com>
2021-08-31 19:51:39 -0400

    updated UI for health screen

--- frontend/screens/Health.js
+++ frontend/screens/Health.js
@@ -3,9 +3,8 @@ import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import CustomCard from '../components/card/custom-card';
 import { MainContext } from '../context/MainContext';
-import CustomHeader from '../components/header/custom-header';
-import { Dimensions } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import IconBadge from '../components/iconBadge/custom-iconBadge';
 
 const Health = ({ navigation }) => {
   const context = useContext(MainContext);
@@ -14,121 +13,183 @@ const Health = ({ navigation }) => {
     return word.charAt(0).toUpperCase() + word.slice(1);
   };
 
-  const containerClass = 'container' + capitalize(context.theme);
-
   return (
     <SafeAreaView>
       <ScrollView>
-        <View style={[styles.container, styles[containerClass]]}>
-          <CustomHeader>Health</CustomHeader>
-          <View style={[styles.wrapper]}>
-            <CustomCard
-              outerStyle={[styles.feature]}
-              innerStyle={{ height: '100%' }}
-            >
-              <LinearGradient
-                start={{ x: 0, y: 0 }}
-                end={{ x: 1, y: 0 }}
-                colors={context.gradient}
-                style={{
-                  borderRadius: 10,
-                  width: '100%',
-                  height: '100%',
-                  padding: 10,
-                }}
-              >
-                <Text style={[styles.header]}>Main Feature</Text>
-              </LinearGradient>
-            </CustomCard>
+        <LinearGradient
+          start={{ x: 0, y: 0 }}
+          end={{ x: 1, y: 0 }}
+          colors={context.gradient}
+          style={styles.mainContainer}
+        >
+          <View style={styles.upperContainer}>
+            <View style={styles.textWrapper}>
+              <IconBadge
+                library="MaterialCommunityIcons"
+                noTouchOpacity={true}
+                color={'#fff'}
+                size={35}
+                icon={'heart-pulse'}
+              />
+              <Text style={styles.title}>Health</Text>
+            </View>
+          </View>
 
+          <CustomCard
+            outerStyle={[
+              styles.lowerOuterContainer,
+              context.theme === 'dark'
+                ? { backgroundColor: '#000000' }
+                : { backgroundColor: '#F8F8F8' },
+            ]}
+            innerStyle={styles.lowerInnerContainer}
+            noTouchOpacity
+          >
             <CustomCard
-              outerStyle={styles.feature}
-              innerStyle={styles.innerView}
+              outerStyle={styles.infoCardOuter}
+              innerStyle={styles.infoCardInner}
               onPress={() => navigation.navigate('VaccinationRecords')}
             >
-              <Text style={[styles.header]}>Vaccination Records</Text>
-              <Text style={[styles.subtitle]}>Vaccination Record Details</Text>
+              <View style={styles.imageContainer}>
+                <Image
+                  source={require('../assets/vaccination-image.png')}
+                  style={styles.image}
+                  resizeMode="contain"
+                />
+              </View>
+              <View style={styles.textContainer}>
+                <Text
+                  style={[
+                    styles.header,
+                    context.theme === 'dark'
+                      ? { color: '#ffffff' }
+                      : { color: '#212121' },
+                  ]}
+                >
+                  Vaccination Records
+                </Text>
+                <Text style={[styles.subtitle]}>
+                  Manage your vaccine passports
+                </Text>
+              </View>
             </CustomCard>
             <CustomCard
-              outerStyle={styles.feature}
-              innerStyle={styles.innerView}
+              outerStyle={styles.infoCardOuter}
+              innerStyle={styles.infoCardInner}
               onPress={() => navigation.navigate('PrescriptionTracking')}
             >
-              <Text style={[styles.header]}>Prescription Tracker</Text>
-              <Text style={[styles.subtitle]}>Dosage Details</Text>
+              <View style={styles.imageContainer}>
+                <Image
+                  source={require('../assets/prescription-image.png')}
+                  style={styles.image}
+                  resizeMode="contain"
+                />
+              </View>
+              <View style={styles.textContainer}>
+                <Text
+                  style={[
+                    styles.header,
+                    context.theme === 'dark'
+                      ? { color: '#ffffff' }
+                      : { color: '#212121' },
+                  ]}
+                >
+                  Prescription Tracker
+                </Text>
+                <Text style={[styles.subtitle]}>
+                  Keep track of all your prescriptions
+                </Text>
+              </View>
             </CustomCard>
-            <CustomCard
-              outerStyle={styles.feature}
-              innerStyle={styles.innerView}
-              onPress={() => navigation.navigate('VisionTest')}
-            >
-              <Text style={[styles.header]}>Vision Test</Text>
-              <Text style={[styles.subtitle]}>Vision Test Description</Text>
-            </CustomCard>
-            <CustomCard
-              outerStyle={styles.feature}
-              innerStyle={styles.innerView}
-              onPress={() => console.log('test')}
-            >
-              <Text style={[styles.header]}>Next Card</Text>
-              <Text
-                style={[styles.subtitle]}
-                onPress={() => console.log('test')}
-              >
-                Subtitle Card
-              </Text>
-            </CustomCard>
-          </View>
-        </View>
+          </CustomCard>
+        </LinearGradient>
       </ScrollView>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  mainContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
     height: '100%',
+  },
+  textWrapper: {
     width: '100%',
-  },
-  containerLight: {
-    backgroundColor: '#F8F8F8',
-  },
-  containerDark: {
-    backgroundColor: '#000000',
-  },
-  wrapper: {
     display: 'flex',
     flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'flex-start',
     alignItems: 'center',
-    marginRight: 20,
+  },
+  title: {
+    fontSize: 30,
+    fontFamily: 'Oxygen-Bold',
+    color: '#fff',
     marginLeft: 20,
+  },
+  infoCardOuter: {
+    width: '100%',
+    marginVertical: 10,
+    paddingHorizontal: 15,
+    paddingVertical: 20,
+    borderRadius: 15,
+  },
+  infoCardInner: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 225,
+  },
+  imageContainer: {
+    flex: 3,
+  },
+  image: {
+    height: 180,
+    width: 180,
+  },
+  textContainer: {
+    flex: 1,
+    justifyContent: 'space-between',
+    alignItems: 'center',
   },
   header: {
     fontSize: 24,
     fontFamily: 'Oxygen-Regular',
-    color: '#D1D1D1', //context based
-    paddingHorizontal: 10,
-    paddingTop: 10,
   },
   subtitle: {
     fontSize: 16,
-    fontFamily: 'Oxygen-Light',
-    color: '#7E7E7E', //context based
-    paddingHorizontal: 10,
-    paddingBottom: 10,
+    fontFamily: 'Oxygen-Regular',
+    color: '#7E7E7E',
   },
-  feature: {
-    //marginHorizontal:'2%',
-    marginVertical: 10,
+  upperContainer: {
     width: '100%',
-    height: 150,
-  },
-  innerView: {
+    display: 'flex',
+    justifyContent: 'flex-end',
     alignItems: 'flex-start',
+    paddingVertical: 30,
+    paddingHorizontal: 30,
   },
-  imageContainer: {},
+  lowerOuterContainer: {
+    width: '100%',
+    height: '100%',
+    borderBottomLeftRadius: 0,
+    borderBottomRightRadius: 0,
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    flex: 4,
+    margin: 0,
+    paddingVertical: 25,
+    paddingHorizontal: 25,
+  },
+  lowerInnerContainer: {
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    minHeight: 400,
+  },
+  inputContainer: {
+    width: '100%',
+    paddingHorizontal: 15,
+  },
 });
 
 export default Health;
