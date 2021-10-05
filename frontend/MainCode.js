ab86796dfccb800c545080d73294135f1fdadd9b
Sathursan <ganasha02@gmail.com>
2021-10-05 00:36:18 -0400

    Cleaned up MainCode

--- frontend/MainCode.js
+++ frontend/MainCode.js
@@ -10,17 +10,6 @@ import AuthenticationScreens from "./screens/authenticationScreens/exportAuthent
 import Navbar from "./components/Navbar/Navbar";
 import CustomToastAlert from "./components/alerts/custom-toast-alert";
 
-const getList = async () => {
-  try {
-    let response = await fetch("http://localhost:5000/list");
-    let json = await response.json();
-    console.log(json);
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-//getList();
 export default function MainCode() {
   const context = useContext(MainContext);
 
@@ -32,7 +21,6 @@ export default function MainCode() {
     "Inter-Light": require("./assets/fonts/Inter-Light.ttf"),
     "Oxygen-Light": require("./assets/fonts/Oxygen-Light.ttf"),
   });
-  const [popupAlert, setPopupAlert] = useState(false);
 
   return (
     loaded && (
@@ -53,16 +41,3 @@ export default function MainCode() {
     )
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#ffffff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  text: {
-    fontFamily: "Oxygen-Regular",
-    color: "white",
-  },
-});
